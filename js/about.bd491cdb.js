d9af0564524c2bd40869a0a328a9b4dc4aec67a8
wuyi0020 <wuyi0020@gmail.com>
2024-01-17 18:21:20 +0800

    Vcg: Update live demo

--- js/about.bd491cdb.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkweek2"]=self["webpackChunkweek2"]||[]).push([[443],{833:function(e,n,u){u.r(n),u.d(n,{default:function(){return k}});var t=u(3396);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function c(e,n){return(0,t.wg)(),(0,t.iD)("div",a,s)}var i=u(89);const o={},f=(0,i.Z)(o,[["render",c]]);var k=f}}]);
-//# sourceMappingURL=about.bd491cdb.js.map
